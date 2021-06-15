e172636d7f0bb1fb17f9379bd28352e0466b3c5e
txluis <85911590+txluis@users.noreply.github.com>
2021-06-14 19:38:57 -0500

    Add files via upload

---
+++ SkiMatrix/SkiMatrix/Path.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkiMatrix
+{
+    public class Path
+    {
+        public List<Nodo> listNodes { get; set; }
+
+        public int high { get; set; }
+
+        public int lenght { get; set; }
+    }
+}
