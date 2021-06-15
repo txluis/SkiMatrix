1b77a248d35d50728a3ab1ef70702bf3bcc990d6
txluis <85911590+txluis@users.noreply.github.com>
2021-06-14 19:36:41 -0500

    Add files via upload

---
+++ SkiMatrix/SkiMatrix/Nodo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkiMatrix
+{
+    public class Nodo
+    {
+        public int value { get; set; }
+        public int row { get; set; }
+        public int col { get; set; }
+        public int rowFather { get; set; }
+        public int colFather { get; set; }
+    }
+}
